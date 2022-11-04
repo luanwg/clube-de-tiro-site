cfda188b634c5ffc8c2a96b3a5df1aadd452c552
Luan Souza <luanwg@hotmail.com>
2022-11-03 23:34:18 -0300

    adding google maps

--- config/cors.php
+++ config/cors.php
@@ -16,6 +16,7 @@ return [
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    //'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
--- resources/views/web/contato.blade.php
+++ resources/views/web/contato.blade.php
@@ -2,26 +2,50 @@
 
 @section('content')
 
-@if (Route::getCurrentRoute()->getName() == "web.contato")
-    @component('web.layouts._components.top', ['title' => 'Fale Conosco', 'text' => 'TEm alguma dúvida ou quer nos visitar? Entre em contato agora mesmo!'])
-    @endcomponent
-    @php $subtitle = "Contatos"; @endphp
-    @section('title', 'Fale Conosco')
-@endif
+@component('web.layouts._components.top', ['title' => 'Fale Conosco', 'text' => 'TEm alguma dúvida ou quer nos visitar? Entre em contato agora mesmo!'])
+@endcomponent
+@section('title', 'Fale Conosco')
+
 <!-- Contato -->
 <div class="content-lg container">
-    <div class="row margin-b-40">
-        <div class="col-sm-6">
-            <h2>{{ $subtitle }}</h2>
-        </div>
-    </div>
-    <!--// end row -->
 
     <div class="row">
 
+        <div class="row">
+            <div class="col-md-6">
+                <div id="map"></div>
+            </div>
+        </div>
 
     </div>
     <!--// end row -->
 </div>
 <!-- End Contato -->
+
+
+ 
+
+<style>
+
+#map {
+    width: 100%;
+}
+
+</style>
+
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzYsvi1JAcnaL3s8gXWG_P10s__KGzwJM&callback=initMap"></script> -->
+<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzYsvi1JAcnaL3s8gXWG_P10s__KGzwJM&callback=initMap&v=weekly" defer></script> -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDzYsvi1JAcnaL3s8gXWG_P10s__KGzwJM"></script>
+
+<script type="text/javascript">
+    let map;
+    window.addEventListener('load', (event) => {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+        });
+    });
+
+</script>
 @endsection
